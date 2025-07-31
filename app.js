e1dad8de06977359d4d73c3b2cbffc9fc6543d22
devansh4399 <devansh4399@gmail.com>
2025-07-31 11:19:32 +0530

    Episode1

---
+++ app.js
@@ -0,0 +1,21 @@
+
+/* <div id="parent">
+      <div id="child">
+         <h1>I am H1</h1>
+         <h2> I am H2</h2>
+
+         </div>
+         </div>
+         */
+        const heading = React.createElement
+        ("div",
+            {id:"parent"},
+         React.createElement("div",{id:"child"},
+            [React.createElement("h1",{},"I am H1 tag"),
+               React.createElement("h2",{},"I am H2 tag")] )
+         );
+
+        const root=ReactDOM.createRoot(document.getElementById("root"));
+
+        root.render(heading);
+
