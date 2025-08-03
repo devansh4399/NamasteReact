0cc0ff16a2e84831ee21099a11b917bfa16a4ea2
devansh4399 <devansh4399@gmail.com>
2025-08-03 20:41:56 +0530

    second

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 /* <div id="parent">
       <div id="child">
          <h1>I am H1</h1>
@@ -11,8 +12,8 @@
         ("div",
             {id:"parent"},
          React.createElement("div",{id:"child"},
-            [React.createElement("h1",{},"I am H1 tag"),
-               React.createElement("h2",{},"I am H2 tag")] )
+           [ React.createElement("h1", { key: "h1" }, "I am H1 tag"),
+            React.createElement("h2", { key: "h2" }, "I am H2 tag")] )
          );
 
         const root=ReactDOM.createRoot(document.getElementById("root"));
