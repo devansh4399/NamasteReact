fc626ae4c78e4d18b0ac5bff4420451c6bb72d73
devansh4399 <devansh4399@gmail.com>
2026-01-16 10:41:09 +0530

    some more test case

--- src/components/__tests__/header.test.js
+++ src/components/__tests__/header.test.js
@@ -2,17 +2,49 @@ import { Provider } from "react-redux";
 import appStore from "../../utils/appStore";
 import { BrowserRouter } from "react-router-dom";
 import HeaderComp from "../HeaderComp";
-import { render,screen } from "@testing-library/react"
+import { fireEvent, render,screen } from "@testing-library/react"
 import "@testing-library/jest-dom"
 
 test("testing the header component",()=>{
-    <BrowserRouter>
+    render( <BrowserRouter>
     <Provider store={appStore}>
-    render(<HeaderComp/>)
+   <HeaderComp/>
     </Provider>
-    </BrowserRouter>
+    </BrowserRouter>);
 
-    const loginButton=screen.getByRole("button");
+    const loginButton = screen.getByRole("button", { name: /login/i });
 
     expect(loginButton).toBeInTheDocument();
+
+  
+});
+
+test("testing the header component with cart items 0",()=>{
+    render( <BrowserRouter>
+    <Provider store={appStore}>
+   <HeaderComp/>
+    </Provider>
+    </BrowserRouter>);
+
+
+    
+});
+
+
+test("should chnage login button to logout",()=>{
+    render( <BrowserRouter>
+    <Provider store={appStore}>
+   <HeaderComp/>
+    </Provider>
+    </BrowserRouter>);
+
+    const loginButton = screen.getByRole("button", { name: /login/i });
+
+    fireEvent.click(loginButton);
+    
+    const logoutButton= screen.getByRole("button",{name:/logout/i});
+
+    expect(logoutButton).toBeInTheDocument();
+
+  
 });
